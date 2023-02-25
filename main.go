810bdc4e209406a6af015457774a90d64cc7e8f5
Ben Walton <bdwalton@debian.lan>
2023-02-25 15:29:11 +0000

    Add the ability to have basic defaults for expected settings.
    For now, we specify a default server.port value to demonstrate the
    concept. Later on, we may drop this and prefer that everything comes
    from the primary config file as specified by --config.

--- main.go
+++ main.go
@@ -1,17 +1,26 @@
 package main
 
 import (
+	_ "embed"
 	"flag"
 	"fmt"
 	"log"
+	"os"
 
 	"gopkg.in/ini.v1"
 )
 
 var (
-	configFile = flag.String("config", "", "Path to the config file. Required.")
+	configFile  = flag.String("config", "", "Path to the config file. Required.")
+	printConfig = flag.Bool("print_config", false, "Print the final config and exit.")
 )
 
+//go:embed default.ini
+var defaultConfig []byte
+
+// validateConfig expects a valid ini.File object and ensure that all
+// of the required settings are valid. A useful error will be returned
+// for any object that doesn't meet the basic requirements.
 func validateConfig(cfg *ini.File) error {
 	if _, err := cfg.Section("server").Key("port").Int(); err != nil {
 		return fmt.Errorf("Invalid server.port setting: %w", err)
@@ -27,12 +36,20 @@ func main() {
 		log.Fatal("You must specify a config file via --config.")
 	}
 
-	cfg, err := ini.Load(*configFile)
+	// defaultConfig first because others take precedence when it
+	// comes to duplicates.
+	cfg, err := ini.Load(defaultConfig, *configFile)
 	if err != nil {
 		log.Fatalf("Failed to load config %q: %v\n", *configFile, err)
 	}
 
 	if err := validateConfig(cfg); err != nil {
 		log.Fatalf("Invalid config: %v\n", err)
+	}
+
+	if *printConfig {
+		if _, err := cfg.WriteTo(os.Stdout); err != nil {
+			log.Fatalf("Error printing config: %v\n", err)
+		}
 	}
 }
