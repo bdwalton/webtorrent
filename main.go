b4a99d69f77486e1849c08ec4c12064beac6b192
Ben Walton <bdwalton@debian.lan>
2023-02-25 14:56:26 +0000

    Add basic config file loading and validation.
    Rely on gopkg.in/ini.v1 for INI file loading support. Supply and
    initial basic config.ini file that specifies server.port. Add
    validateConfig() to do validations that ensure we have all of the
    required settings to fire up.

--- main.go
+++ main.go
@@ -3,13 +3,36 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
+
+	"gopkg.in/ini.v1"
 )
 
 var (
 	configFile = flag.String("config", "", "Path to the config file. Required.")
 )
 
+func validateConfig(cfg *ini.File) error {
+	if _, err := cfg.Section("server").Key("port").Int(); err != nil {
+		return fmt.Errorf("Invalid server.port setting: %w", err)
+	}
+
+	return nil
+}
+
 func main() {
 	flag.Parse()
-	fmt.Printf("Config file: %s\n", *configFile)
+
+	if *configFile == "" {
+		log.Fatal("You must specify a config file via --config.")
+	}
+
+	cfg, err := ini.Load(*configFile)
+	if err != nil {
+		log.Fatalf("Failed to load config %q: %v\n", *configFile, err)
+	}
+
+	if err := validateConfig(cfg); err != nil {
+		log.Fatalf("Invalid config: %v\n", err)
+	}
 }
