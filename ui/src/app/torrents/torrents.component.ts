8d5f937fb52b29e8fa4d0b6819c099ec4009c57e
Ben Walton <bdwalton@gmail.com>
2023-02-28 15:24:29 +0000

    Add angular scaffolding for a torrents component.
    Signed-off-by: Ben Walton <bdwalton@gmail.com>

---
+++ ui/src/app/torrents/torrents.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-torrents',
+  templateUrl: './torrents.component.html',
+  styleUrls: ['./torrents.component.scss']
+})
+export class TorrentsComponent {
+
+}
