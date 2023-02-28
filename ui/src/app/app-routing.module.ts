b647447048ba504116bbe9be32bf2bce89e9d8c6
Ben Walton <bdwalton@gmail.com>
2023-02-28 22:42:05 +0000

    Setup routing to use the components we've added.

--- ui/src/app/app-routing.module.ts
+++ ui/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
-import { NgModule } from '@angular/core';
+import { HomeComponent } from './home/home.component';
 import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { TorrentComponent } from './torrent/torrent.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'torrent', component: TorrentComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
