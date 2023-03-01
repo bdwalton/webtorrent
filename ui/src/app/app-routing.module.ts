e146a2bdecd35eec89b96d073193ebd1a79e2a6f
Ben Walton <bdwalton@gmail.com>
2023-03-01 15:08:53 +0000

    Teach the routing config how to title the app.

--- ui/src/app/app-routing.module.ts
+++ ui/src/app/app-routing.module.ts
@@ -3,8 +3,16 @@ import { NgModule } from '@angular/core';
 import { TorrentComponent } from './torrent/torrent.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'torrent', pathMatch: 'full' },
-  { path: 'torrent', component: TorrentComponent },
+  {
+    path: '',
+    redirectTo: 'torrent',
+    pathMatch: 'full'
+  },
+  {
+    path: 'torrent',
+    component: TorrentComponent,
+    title: "WebTorrent - Torrents"
+  },
 ];
 
 @NgModule({
