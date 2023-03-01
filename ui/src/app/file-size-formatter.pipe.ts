0875ec194e7ff201386713f338c789f20380d8ec
Ben Walton <bdwalton@gmail.com>
2023-03-01 14:30:44 +0000

    Add the code for the file size formatting pipe.

--- ui/src/app/file-size-formatter.pipe.ts
+++ ui/src/app/file-size-formatter.pipe.ts
@@ -1,12 +1,20 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
+const FILE_SIZE_UNITS = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+
 @Pipe({
   name: 'fileSizeFormatter'
 })
 export class FileSizeFormatterPipe implements PipeTransform {
+  transform(sizeInBytes: number): string {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+    let power = Math.round(Math.log(sizeInBytes) / Math.log(1024));
+    power = Math.min(power, FILE_SIZE_UNITS.length - 1);
+
+    const size = sizeInBytes / Math.pow(1024, power); // size in new units
+    const formattedSize = Math.round(size * 100) / 100; // keep up to 2 decimals
+    const unit = FILE_SIZE_UNITS[power];
+
+    return `${formattedSize} ${unit}`;
   }
-
 }
