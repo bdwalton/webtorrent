729000986e3d7cdeaba8caa7ee98109dae06b9be
Ben Walton <bdwalton@gmail.com>
2023-03-01 15:10:24 +0000

    Add some (initally failing) tests for the File Size pipe formatter.

--- ui/src/app/file-size-formatter.pipe.spec.ts
+++ ui/src/app/file-size-formatter.pipe.spec.ts
@@ -1,8 +1,23 @@
 import { FileSizeFormatterPipe } from './file-size-formatter.pipe';
 
 describe('FileSizeFormatterPipe', () => {
-  it('create an instance', () => {
+  it('transforms 518285851 to "0.48 GiB"', () => {
     const pipe = new FileSizeFormatterPipe();
-    expect(pipe).toBeTruthy();
+    expect(pipe.transform(518285851)).toBe("0.48 GiB");
+  });
+
+  it('transforms 0 to "0 B"', () => {
+    const pipe = new FileSizeFormatterPipe();
+    expect(pipe.transform(51)).toBe("51 B");
+  });
+
+  it('transforms 1 to "1 B"', () => {
+    const pipe = new FileSizeFormatterPipe();
+    expect(pipe.transform(51)).toBe("51 B");
+  });
+
+  it('transforms 1024 to "1 KiB"', () => {
+    const pipe = new FileSizeFormatterPipe();
+    expect(pipe.transform(1024)).toBe("1024 KiB");
   });
 });
