2b131bd6354cf95f3ac7f2ae39eb49519b36cd31
Ben Walton <bdwalton@gmail.com>
2023-03-05 13:21:29 +0000

    Turn the torrent details view into a sortable material table.
    Use a material table to display file info instead of a hand-crafted
    one. Allow for sorting based on the path/filename.

--- ui/src/app/torrent-details/torrent-details.component.ts
+++ ui/src/app/torrent-details/torrent-details.component.ts
@@ -1,22 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { LiveAnnouncer } from '@angular/cdk/a11y';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute, ParamMap } from '@angular/router';
+import { MatSort, Sort } from '@angular/material/sort';
 import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
-import { TorrentService, TorrentDetails } from '../torrent.service';
+import { MatTableDataSource } from '@angular/material/table';
+import {
+  TorrentService,
+  TorrentDetails,
+  TorrentFile,
+} from '../torrent.service';
 
 @Component({
   selector: 'app-torrent-details',
   templateUrl: './torrent-details.component.html',
   styleUrls: ['./torrent-details.component.scss'],
 })
-export class TorrentDetailsComponent implements OnInit {
+export class TorrentDetailsComponent implements OnInit, AfterViewInit {
   hash: string = '';
   torrent: TorrentDetails = new TorrentDetails();
+  torrentFiles = new MatTableDataSource<TorrentFile>([]);
+
+  displayedColumns: string[] = ['Path', 'Progress'];
 
   constructor(
     private route: ActivatedRoute,
     private sanitizer: DomSanitizer,
-    private torrentService: TorrentService
+    private torrentService: TorrentService,
+    private _liveAnnouncer: LiveAnnouncer
   ) {}
+
+  // Must be set in the component html or this will be undefined.
+  @ViewChild(MatSort) sort!: MatSort;
 
   ngOnInit() {
     this.route.paramMap.subscribe((params: ParamMap) => {
@@ -26,8 +40,21 @@ export class TorrentDetailsComponent implements OnInit {
     this.getTorrentDetails(this.hash);
   }
 
+  ngAfterViewInit() {
+    this.torrentFiles.sort = this.sort;
+  }
+
   sanitize(url: string) {
     return this.sanitizer.bypassSecurityTrustUrl(url);
+  }
+
+  /** Announce the change in sort state for assistive technology. */
+  announceSortChange(sortState: Sort) {
+    if (sortState.direction) {
+      this._liveAnnouncer.announce(`Sorted ${sortState.direction}ending`);
+    } else {
+      this._liveAnnouncer.announce('Sorting cleared');
+    }
   }
 
   getTorrentDetails(hash: string) {
@@ -35,6 +62,7 @@ export class TorrentDetailsComponent implements OnInit {
       .getTorrentDetails(hash)
       .subscribe((data: TorrentDetails) => {
         this.torrent = data;
+        this.torrentFiles.data = [...this.torrent.Files];
       });
   }
 }
