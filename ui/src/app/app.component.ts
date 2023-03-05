614dcf485adf6421deb2b0e2a3b237c1ab81fb28
Ben Walton <bdwalton@gmail.com>
2023-03-05 16:07:31 +0000

    Remove unused ActivatedRoute object from app component

--- ui/src/app/app.component.ts
+++ ui/src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'WebTorrent';
 
-  constructor(private activatedRoute: ActivatedRoute) { }
+  constructor() {}
 }
