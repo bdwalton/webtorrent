c593ee99bb7e35a7c756002852a38bff61e687aa
Ben Walton <bdwalton@gmail.com>
2023-03-05 15:57:35 +0000

    Make the HTTP interceptor throw up a snackbar error message.
    The interceptor will now throw up an error message for the user to see
    so there is a visible response. It persists for 5 seconds and is then
    closed. We don't do additional logging (other default handlers do
    that) and just rethrow the error.

--- ui/src/app/http-errors.interceptor.ts
+++ ui/src/app/http-errors.interceptor.ts
@@ -1,18 +1,44 @@
 import { Injectable } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import {
   HttpRequest,
   HttpHandler,
   HttpEvent,
-  HttpInterceptor
+  HttpInterceptor,
+  HttpErrorResponse,
 } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 @Injectable()
 export class HttpErrorsInterceptor implements HttpInterceptor {
+  constructor(private _snackBar: MatSnackBar) {}
 
-  constructor() {}
+  intercept(
+    request: HttpRequest<unknown>,
+    next: HttpHandler
+  ): Observable<HttpEvent<unknown>> {
+    return next.handle(request).pipe(
+      catchError((err) => {
+        // Note that we don't do logging here. We leave that to
+        // default event handling. We only intercept errors so we can
+        // show a snack bar to the user.
+        if (err.error instanceof ErrorEvent) {
+          // Client side.
+          this._snackBar.open('Local error: ' + err.error, 'Ok', {
+            duration: 5000,
+          });
+        } else {
+          // Server side.
+          this._snackBar.open('Remote error: ' + err.error.Detail, 'Ok', {
+            duration: 5000,
+          });
+        }
 
-  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    return next.handle(request);
+        // We only wanted to do user notification cleanly, so rethrow
+        // the error for default, upstream handling.
+        return throwError(err);
+      })
+    );
   }
 }
