1f0896f7326de0ad7160f24581949a9c984c103f
Ben Walton <bdwalton@gmail.com>
2023-03-05 17:51:01 +0000

    Switch to using the shared progress component for consistency
    We convert both the torrent list and specific torrent details view to
    use the progress component. Now, we can improve a few things, like
    percentage display for torrents that don't yet have info, in a single
    place.

--- ui/src/app/progress-component/progress-component.component.ts
+++ ui/src/app/progress-component/progress-component.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Progress } from '../torrent.service';
 
 @Component({
   selector: 'app-progress-component',
   templateUrl: './progress-component.component.html',
-  styleUrls: ['./progress-component.component.scss']
+  styleUrls: ['./progress-component.component.scss'],
 })
 export class ProgressComponentComponent {
-
+  @Input() prg = new Progress(); // A torrent or file progress
 }
