b0ae656736ec2255d2ec65c83e5598794d30fb54
Ben Walton <bdwalton@gmail.com>
2023-03-05 12:03:38 +0000

    Add the basic elements of the new Torrent Details component.
    We'll start to display file information for the torrent in a simple
    way for now.

--- ui/src/app/torrent-details/torrent-details.component.ts
+++ ui/src/app/torrent-details/torrent-details.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { TorrentService, TorrentDetails } from '../torrent.service';
 
 @Component({
   selector: 'app-torrent-details',
   templateUrl: './torrent-details.component.html',
   styleUrls: ['./torrent-details.component.scss']
 })
-export class TorrentDetailsComponent {
+export class TorrentDetailsComponent implements OnInit {
+  hash: string = ''
+  torrent: TorrentDetails = new TorrentDetails();
 
+  constructor(private route: ActivatedRoute,
+              private torrentService: TorrentService) { }
+
+  ngOnInit() {
+    this.route.paramMap.subscribe((params: ParamMap) => {
+      this.hash = params.get('hash') as string;
+    });
+
+    this.getTorrentDetails(this.hash);
+  }
+
+  getTorrentDetails(hash: string) {
+    this.torrentService.getTorrentDetails(hash).subscribe((data: TorrentDetails) => {
+      this.torrent = data
+    });
+  }
 }
